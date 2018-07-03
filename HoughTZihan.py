1e8df8f272668d7927fa133f931efa758f3a2f67
henry <henrysteinsgate@outlook.com>
2018-07-03 13:49:37 -0400

    HoughMethod

---
+++ HoughTZihan.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import argparse as ap
+
+# Get the path of the training set
+parser = ap.ArgumentParser()
+parser.add_argument("-i", "--image", help="Path to Image", required="True")
+args = vars(parser.parse_args())
+
+# Load the image
+img = cv2.imread(args["image"])
+img = img.copy()
+
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,50,150,apertureSize = 3)
+
+lines = cv2.HoughLinesP(edges,1,np.pi/180,threshold=18,minLineLength=8,maxLineGap=20)
+# for rho,theta in lines[0]:
+#     a = np.cos(theta)
+#     b = np.sin(theta)
+#     x0 = a*rho
+#     y0 = b*rho
+#     x1 = int(x0 + 1000*(-b))
+#     y1 = int(y0 + 1000*(a))
+#     x2 = int(x0 - 1000*(-b))
+#     y2 = int(y0 - 1000*(a))
+#     cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+for line in lines:
+    for x1,y1,x2,y2 in line:
+        cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+
+cv2.imshow("Edges Labeled", img)
+cv2.waitKey()
+cv2.destroyAllWindows()
